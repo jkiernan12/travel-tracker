5e997cb98685e5ac81251450da3798904e15cc2b
John Kiernan <jkiernan12@yahoo.com>
2021-11-15 08:42:18 -0500

    Add accessibility features and styling

--- src/render-dom.js
+++ src/render-dom.js
@@ -1,23 +1,61 @@
-const pendingSection = document.querySelector("#pendingSection");
-const pastSection = document.querySelector("#pastSection");
-const futureSection = document.querySelector("#futureSection");
-const currentSection = document.querySelector("#currentSection");
+const pendingSectionTrips = document.querySelector("#pendingSectionTrips");
+const pastSectionTrips = document.querySelector("#pastSectionTrips");
+const futureSectionTrips = document.querySelector("#futureSectionTrips");
+const currentSectionTrips = document.querySelector("#currentSectionTrips");
 const totalYearSpent = document.querySelector("#totalYearSpent");
 const userGreeting = document.querySelector("#userName");
+const navBar = document.querySelector("#navBar");
+const menuItems = document.querySelectorAll("#navBar li");
+const allSections = document.querySelectorAll("section");
+
+navBar.addEventListener('click', switchSection);
+navBar.addEventListener('keyup', checkEnterKey);
+
+function checkEnterKey(e) {
+  if (e.keyCode === 13) {
+    switchSection(e);
+  }
+}
+
+function switchSection(e) {
+  const clickedSection = document.querySelector(`#${e.target.dataset.section}`);
+  if (e.target.dataset.section) {
+    toggleMenuItem(e.target);
+    allSections.forEach(section => hideElement(section));
+    showElement(clickedSection);
+  }
+}
+
+function hideElement(element) {
+  element.classList.add("hidden");
+  element.ariaHidden = true;
+}
+function showElement(element) {
+  element.classList.remove("hidden");
+  element.ariaHidden = false;
+}
+
+function toggleMenuItem(element) {
+  menuItems.forEach(el => el.classList.remove("selected"));
+  element.classList.add("selected");
+}
 
 function renderUserPage(user) {
   totalYearSpent.innerHTML = "$" + user.lastYearCost();
   userGreeting.innerText = user.name;
-  renderSection(pendingSection, user.getPending());
-  renderSection(pastSection, user.getTripsByDate("past"));
-  renderSection(futureSection, user.getTripsByDate("future"));
-  renderSection(currentSection, user.getTripsByDate("current"));
+  renderSection(pendingSectionTrips, user.getPending());
+  renderSection(pastSectionTrips, user.getTripsByDate("past"));
+  renderSection(futureSectionTrips, user.getTripsByDate("future"));
+  renderSection(currentSectionTrips, user.getTripsByDate("current"));
 }
 
 function renderWidget(trip) {
+  const city = trip.destinationName.split(", ")[0];
+  const region = trip.destinationName.split(", ")[1];
   return `<article>
-    <img src="${trip.img}" alt="${trip.alt}"/>
-    <h4>${trip.destinationName}</h4>
+    <h4>${city}</h4>
+      <img src="${trip.img}" alt="${trip.alt}"/>
+    <p class="region">${region}</p>
     <p>${trip.date}</p>
     <p>${trip.numTravelers} travelers</p>
     <p>Total Cost: $${trip.getTotalCost()}</p>
